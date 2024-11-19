48db5ea72af51cb3e8a47681b8739c0bbc57eec5
Duy18ND <duy18namdinh@gmail.com>
2024-11-19 11:44:30 +0700

    Add files via upload

---
+++ Sesion02-02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	//khai bao bien num1, num2 
+	int num1 = 20;
+	int num2 = 13; 
+	
+	//tinh tong num1 + num2
+	int sum = num1 + num2; 
+	printf("ket qua tong la:%d + %d = %d",num1,num2,sum); 
+	return 0; 
+} 
