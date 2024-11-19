bdfd76d4a044c0114f9fac3f2d496658cf69b6cb
Duy18ND <duy18namdinh@gmail.com>
2024-11-19 11:36:13 +0700

    Add files via upload

---
+++ Sesion02-01.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+	main(){
+	
+	//int la kieu du lieu so nguyen VD:
+	int age = 20; // ket qua cua bien age la: 20
+	
+	//double là kieu du lieu dau phay dong và co do chinh xac KEP VD: 
+	 double bt1 = 20.31; //ket qua cua bt1 la: 20.31 
+	 
+	//float la kieu du lieu luu tru so thuc dau phay dong va co do chinh xac DON VD: 
+	float bt2 = 1.3123 //ket qua cua bt2 la: 1.3123 
+	 
+	 //char là kieu du lieu chua ky tu  VD:
+	 char kytu = 'A'; //ket qua kytu là: A  (luu y kieu du lieu char ky tu thuong de trong ngoac don)
+	 
+	 //short là kieu du lieu ngan dung de luu tru so nguyen 	VD:
+	 short nam = 2024; // ket qua la nam 2024
+	  
+	  //long la kieu du lieu so nguyen dai VD:
+	  long so1 =  122345678910; (ket qua la so1 12345678910) 
+	  
+	   
+	  
+}
