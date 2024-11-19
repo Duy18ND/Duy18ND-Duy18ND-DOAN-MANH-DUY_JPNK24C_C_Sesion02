64a98e27aebc37c1c910b6f78ef95b4ea7ec84cc
Duy18ND <duy18namdinh@gmail.com>
2024-11-19 12:29:36 +0700

    Add files via upload

---
+++ Sesion02-05.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	//chieu dai num1    chieu rong num2 
+	int num1 = 10;
+	int num2 = 15;
+	int chuvi = 2 * (num1 +num2);
+	printf("chu vi hinh chu nhat la: %d",chuvi);
+	
+	int dientich = num1 * num2;
+	printf("\n dien tich hinh chu nhat la: %d",dientich);
+	
+	return 0; 	 
+} 
