64232943594380c6bd7f52e06ebe57b8d56e0922
Duy18ND <duy18namdinh@gmail.com>
2024-11-19 12:17:44 +0700

    Add files via upload

---
+++ Sesion02-04.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+int  canh = 5;
+// tính chu vi hinh vuong canh * 4 
+int chuvihinhvuong = canh * 4;
+printf("chu vi hinh vuong la: %d",chuvihinhvuong);
+int dientichhinhvuong = canh * canh;
+printf("\n dien tich hinh vuong la: %d",dientichhinhvuong);
+return 0; 
+}
